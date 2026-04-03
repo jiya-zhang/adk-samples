55108872be7ccdb2c243ef257b75e132388be222
Maggie Zhang <jiya.zhang.98@gmail.com>
2026-04-03 15:21:57 -0700

    add calendar, email, monday mcp server and clean up

--- python/agents/workato-agent/workato-agent/agent.py
+++ python/agents/workato-agent/workato-agent/agent.py
@@ -1,68 +1,74 @@
 import os
 import dotenv
 from google.adk.agents import LlmAgent
-from google.adk.models import Gemini
-from google.cloud import secretmanager
+from google.cloud.secretmanager import SecretManagerServiceClient
 from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
 from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
 
 dotenv.load_dotenv()
+os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
 
 system_instruction = """
-You are an AI assistant that has access to a list of tools provided by a Workato MCP server.
-When a user requests something, always use the tools from the provided MCP server to perform that task.
+You are an AI assistant that has access to a list of tools provided by Workato MCP servers.
+When a user requests something, always use the tools from the appropriate MCP server to perform that task.
+
+Each Workato MCP server provides tools for one system. You have access to the following tools:
+calendar_toolset: manages Google Calendar
+email_toolset: manages Gmail
+monday_toolset: manages Monday.com
 """
 
-
-def get_workato_mcp_toolset(mcp_token=None, mcp_url=None):
-    if not mcp_token:
-        print("Warning: MCP token not found.")
-        return
-    if not mcp_url:
+# MCP URL should already include Workato Developer MCP token
+# in the format of "?wkt_token=xxx"
+def get_mcp_toolset(mcp_url_with_token=None):
+    if not mcp_url_with_token:
         print("Warning: MCP URL not found.")
         return
 
     tools = MCPToolset(
         connection_params=StreamableHTTPConnectionParams(
-            url=mcp_url,
-            headers={
-                "Authorization": f"Bearer {mcp_token}"
-            },
+            url=mcp_url_with_token,
             timeout=60,
         )
     )
-    print("DEBUG tools retrieved")
     return tools
 
 
-def get_workato_mcp_token(project_id, secret_id):
-    """Retrieves the Workato MCP token from Google Cloud Secret Manager."""
+def get_mcp_url(project_id, secret_id):
+    """When running remotely, retrieve the Workato MCP token from Google Cloud Secret Manager."""
     if not project_id:
         raise ValueError("GOOGLE_CLOUD_PROJECT environment variable not set.")
 
     secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-    client = secretmanager.SecretManagerServiceClient()
+    client = SecretManagerServiceClient()
     response = client.access_secret_version(request={"name": secret_name})
-    print(f"DEBUG token:{response.payload.data.decode('UTF-8')}")
+
     return response.payload.data.decode("UTF-8")
 
 
 
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
-SECRET_ID = os.getenv("WORKATO_SECRET_ID")
-WORKATO_MCP_URL = os.getenv("WORKATO_MCP_URL")
-WORKATO_MCP_TOKEN = get_workato_mcp_token(PROJECT_ID,SECRET_ID)
+LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
 
-print(f"DEBUG WORKATO_MCP_TOKEN: {WORKATO_MCP_TOKEN}")
-print(f"DEBUG WORKATO_MCP_URL: {WORKATO_MCP_URL}")
-print(f"DEBUG PROJECT_ID: {PROJECT_ID}")
-print(f"DEBUG SECRET_ID: {SECRET_ID}")
+# Each MCP Server has tools to access one system
+# Local runtime uses _MCP_URL to authenticate
+# Remote runtime uses _SECRET_ID to access the URL from Secret Manager and then authenticate
+CALENDAR_MCP_URL = os.getenv("CALENDAR_MCP_URL")
+CALENDAR_SECRET_ID = os.getenv("CALENDAR_SECRET_ID")
+EMAIL_MCP_URL = os.getenv("EMAIL_MCP_URL")
+EMAIL_SECRET_ID = os.getenv("EMAIL_SECRET_ID")
+MONDAY_MCP_URL = os.getenv("MONDAY_MCP_URL")
+MONDAY_SECRET_ID = os.getenv("MONDAY_SECRET_ID")
 
-workato_toolset = get_workato_mcp_toolset(WORKATO_MCP_TOKEN, WORKATO_MCP_URL)
+
+calendar_toolset = get_mcp_toolset(CALENDAR_MCP_URL)
+email_toolset = get_mcp_toolset(EMAIL_MCP_URL)
+monday_toolset = get_mcp_toolset(MONDAY_MCP_URL)
+
 
 root_agent = LlmAgent(
-    model=Gemini(model="gemini-3-flash-preview"),
+    model="gemini-3-flash-preview",
     name="workato_agent",
     instruction=system_instruction,
-    tools=[workato_toolset],
+    tools=[calendar_toolset,email_toolset,monday_toolset],
 )
